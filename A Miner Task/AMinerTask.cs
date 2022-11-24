aac8bca6dc3f1bab65041b03073482e8746d71cb
DimitrovVladimir <vmdimitrow@gmail.com>
2022-11-24 14:57:13 +0200

    Dictionaries

---
+++ A Miner Task/AMinerTask.cs
@@ -0,0 +1,33 @@
+﻿namespace A_Miner_Task
+{
+    internal class AMinerTask
+    {
+        static void Main(string[] args)
+        {
+            var resourcesQuantities = new Dictionary<string, decimal>();
+
+            var line = Console.ReadLine();
+
+            while (line != "stop")
+            {
+                var resource = line;
+                var quantity = decimal.Parse(Console.ReadLine());
+                if (!resourcesQuantities.ContainsKey(resource))
+                {
+                    resourcesQuantities[resource] = 0;
+                }
+
+                resourcesQuantities[resource] += quantity;
+
+                line = Console.ReadLine();
+
+            }
+            foreach (var resourceQuantity in resourcesQuantities)
+            {
+                var resource =resourceQuantity.Key;
+                var quantity = resourceQuantity.Value;
+                Console.WriteLine($"{resource} -> {quantity}");
+            }
+        }
+    }
+}
---
+++ Logs Aggregator/LogsAggregator.cs
@@ -0,0 +1,41 @@
+﻿namespace Logs_Aggregator
+{
+    internal class LogsAggregator
+    {
+        static void Main(string[] args)
+        {
+            var namesIpsDurations =new SortedDictionary<String, SortedDictionary<String, int>>();
+
+            var count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                var line = Console.ReadLine();
+
+                var tokens = line.Split();
+
+                var name = tokens[1];
+                var ip = tokens[0];
+                var duration = int.Parse(tokens[2]);
+
+                if (!namesIpsDurations.ContainsKey(name))
+                {
+                    namesIpsDurations[name] = new SortedDictionary<String, int>();
+                }
+                if (!namesIpsDurations[name].ContainsKey(ip))
+                {
+                    namesIpsDurations[name][ip] = 0;
+                }
+                namesIpsDurations[name][ip] += duration;
+            }
+            foreach (var nameIpDurations in namesIpsDurations) 
+            { 
+                var name = nameIpDurations.Key;
+                var ipsDurations = nameIpDurations.Value;
+
+                var totalDuration = ipsDurations.Sum(a => a.Value);
+                var ips = ipsDurations.Select(a => a.Key).ToArray();
+                Console.WriteLine($"{name}: {totalDuration} [{String.Join (", ", ips)}]");
+            }
+        }
+    }
+}
